e93ce60707c6ea4b29231b7fe5cebaf6aa071ba5
Wichoski <christianwichoski@gmail.com>
2023-02-12 03:56:35 -0300

    criação novos arquivos de teste

---
+++ MeuProjeto/src/TiposDeVariaveis/TiposDeVariaveis.java
@@ -0,0 +1,16 @@
+package TiposDeVariaveis;
+
+public class TiposDeVariaveis {
+
+public static void main(String[] args)throws Exception {
+	String meuNome = "Christian Luis Wichoski ";
+	int idade = 28 ;
+	double peso = 128.5 ;
+	
+	System.out.print(meuNome );
+	System.out.print(idade );
+	System.out.print(peso );
+	
+}
+
+}
